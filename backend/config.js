e294f2db32f1bc0f3eaca96b124294ef0c4ea0ca
Akshu <144461057+Akshu023@users.noreply.github.com>
2026-04-10 11:59:12 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/e-commerce");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
