fb383f6b49c6fa2ad40ef9200b42bfe21b182260
Akshu <144461057+Akshu023@users.noreply.github.com>
2026-04-10 12:35:07 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -152,7 +152,8 @@ app.delete('/product/:id', async (req, resp) => {
 });
 
 
-const PORT = 1709;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
