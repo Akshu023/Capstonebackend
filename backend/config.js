d906331725a622707887a870690d86f3bf31a37c
Akshu <144461057+Akshu023@users.noreply.github.com>
2026-04-10 12:41:14 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ MongoDB error:", err));
 
